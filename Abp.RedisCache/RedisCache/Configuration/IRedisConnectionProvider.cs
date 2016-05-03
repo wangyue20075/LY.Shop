ff1e45ca90f4af9e5fa279509153276f3328a793
place <place@DESKTOP-MI50PPC>
2016-05-04 01:57:28 +0800

    first

---
+++ Abp.RedisCache/RedisCache/Configuration/IRedisConnectionProvider.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+namespace Abp.RedisCache.Configuration
+{
+    public interface IAbpRedisConnectionProvider 
+    {
+        ConnectionMultiplexer GetConnection(string connectionString);
+
+        string GetConnectionString(string name);
+        
+        int GetDatabaseId(string name);
+    }
+}
